f018d535f8594a06e0e5cefe600840fe552e8ae3
Kiran Kumar <kirankumar021097@gmail.com>
2022-08-24 14:26:39 +0530

    Admin login done

--- backend/crud/admin.py
+++ backend/crud/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Users
 
 # Register your models here.
+@admin.register(Users)
 class UsersModel(admin.ModelAdmin):
     list_filter = ('id','name','email')
     list_display = ('id', 'name', 'email')
