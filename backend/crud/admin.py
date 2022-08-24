1be3ce378dcf1bd4dddfb532ebdd428f18712141
Kiran Kumar <kirankumar021097@gmail.com>
2022-08-24 14:08:47 +0530

    Migrations and models done

--- backend/crud/admin.py
+++ backend/crud/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Users
 
 # Register your models here.
+class UsersModel(admin.ModelAdmin):
+    list_filter = ('id','name','email')
+    list_display = ('id', 'name', 'email')
--- backend/crud/models.py
+++ backend/crud/models.py
@@ -1,3 +1,10 @@
+import email
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return self.name
